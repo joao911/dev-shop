71af1074c8835a7a3d52e766dd5c855cead8e2e9
joao911 <joaopaulolacerda911@gmail.com>
2026-05-05 08:23:30 -0300

    feat: project is done

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,33 +1,62 @@
+import { api } from "@/data/api";
+import { Product } from "@/data/types/products";
 import Image from "next/image";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default async function Search() {
+interface SearchPageProps {
+  searchParams: {
+    q?: string;
+  };
+}
+
+async function searchProducts(query: string): Promise<Product[]> {
+  const response = await api(`/products/search?q=${query}`, {
+    next: {
+      revalidate: 60 * 5,
+    },
+  });
+  const products = await response.json();
+  return products;
+}
+
+export default async function Search({ searchParams }: SearchPageProps) {
+  const { q: query } = await searchParams;
+
+  const products = await searchProducts(query || "");
+
+  console.log("query", products);
+  if (!query) {
+    redirect("/");
+  }
   return (
     <div className="flex flex-col gap-4">
       <p>
-        Resultados para <span className="font-semibold">Moleton</span>
+        Resultados para <span className="font-semibold">{query}</span>
       </p>
       <div className="grid grid-cols-3 gap-6">
-        <Link
-          href={`/product/moleton-never-stop-learning`}
-          className="relative group  rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-        >
-          <Image
-            src="/moletom-never-stop-learning.png"
-            className="group-hover:scale-105 transition-transform duration-500"
-            width={480}
-            height={480}
-            quality={100}
-            alt=""
-          />
+        {products.map((item) => (
+          <Link
+            href={`/product/${item.slug}`}
+            className="relative group  rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+          >
+            <Image
+              src={item.image}
+              className="group-hover:scale-105 transition-transform duration-500"
+              width={480}
+              height={480}
+              quality={100}
+              alt=""
+            />
 
-          <div className="absolute rounded-full bottom-28 right-28 h-12 flex items-center gap-2 max-w-70 border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-            <span className="text-sm truncate">Moleton</span>
-            <span className="flex w-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-              R$ 180
-            </span>
-          </div>
-        </Link>
+            <div className="absolute rounded-full bottom-28 right-28 h-12 flex items-center gap-2 max-w-70 border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+              <span className="text-sm truncate">{item.title}</span>
+              <span className="flex w-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                R$ {item.price}
+              </span>
+            </div>
+          </Link>
+        ))}
       </div>
     </div>
   );
--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { Search, ShoppingBag } from "lucide-react";
 import Image from "next/image";
 import CardBadge from "./components/CardBadge";
+import SearchForm from "./components/SearchForm";
 
 export default function Header() {
   return (
@@ -10,15 +10,7 @@ export default function Header() {
         <Link href="/" className="text-2xl font-extrabold text-white">
           DevStore
         </Link>
-
-        <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
-          <Search className="w-5 h-5 text-zinc-500" />
-
-          <input
-            placeholder="Buscar produtos..."
-            className="flex-1 bg-transparent text-xs outline-none placeholder:text-zinc-500"
-          />
-        </form>
+        <SearchForm />
       </div>
       <div className="flex items-center gap-4">
         <CardBadge />
